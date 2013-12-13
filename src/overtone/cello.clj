9c3814e354ef901fdc5af1b98fbbcce608335ceb
Joseph Wilk <joe@josephwilk.net>
2013-12-13 23:35:37 +0100

    samples -> buffers

---
+++ src/overtone/cello.clj
@@ -0,0 +1,21 @@
+(ns overtone.orchestra.cello
+  (:use [overtone.live]))
+
+(def cello-dir "/Users/josephwilk/Workspace/music/samples/instruments/cello/wavs/")
+(def cello-samples (remove nil? (map #(try (load-sample %)
+                                           (catch Exception e nil)) (file-seq (clojure.java.io/file cello-dir)))))
+
+(definst cello
+  "length: 1, 1/2 1/15 1/25
+   valid modes: * :forte-arco-normal
+                * :fortissimo-arco-normal
+                * :mezzo-piano-arco-normal
+                * :mezzo-piano-non-vibrato
+                * :pianissimo-arco-normal
+"
+  [note 60 length 1 mode :forte-acro-normal]
+  (let [buf (index:kr (:id index-buffer) note)
+        env (env-gen (adsr attack decay sustain release level curve)
+                     :gate gate
+                     :action FREE)]
+    (* env (scaled-play-buf 2 buf :level level :loop loop? :action FREE))))
