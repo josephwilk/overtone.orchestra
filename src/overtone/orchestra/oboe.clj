58eb65cd2a9d9d74e7345989868cae40bf3678ce
Joseph Wilk <joe@josephwilk.net>
2013-12-16 20:09:52 +0100

    TODO: oboe

---
+++ src/overtone/orchestra/oboe.clj
@@ -0,0 +1,7 @@
+(ns overtone.orchestra.oboe
+    Samples: wget http://www.philharmonia.co.uk/assets/audio/samples/oboe/oboe.zip
+  "
+  (:use [overtone.live]))
+
+(def oboe-dir "/Users/josephwilk/Workspace/music/samples/instruments/oboe/wavs")
+
