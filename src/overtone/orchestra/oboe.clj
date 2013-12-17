bff734fb022ca63bb7e7b92c4f18ce863662b061
Joseph Wilk <joe@josephwilk.net>
2013-12-17 23:15:49 +0100

    Update oboe.clj

--- src/overtone/orchestra/oboe.clj
+++ src/overtone/orchestra/oboe.clj
@@ -1,6 +1,5 @@
 (ns overtone.orchestra.oboe
-    Samples: wget http://www.philharmonia.co.uk/assets/audio/samples/oboe/oboe.zip
-  "
+  "Samples: wget http://www.philharmonia.co.uk/assets/audio/samples/oboe/oboe.zip"
   (:use [overtone.live]))
 
 (def oboe-dir "/Users/josephwilk/Workspace/music/samples/instruments/oboe/wavs")
